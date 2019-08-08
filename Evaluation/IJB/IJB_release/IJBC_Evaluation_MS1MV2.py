4f4db7c255c2693a391ee2737506b2771ada6418
quyan <quyan.bupt@qq.com>
2019-08-09 00:45:48 +0800

    [update] Do not show plt.

--- Evaluation/IJB/IJB_release/IJBC_Evaluation_MS1MV2.py
+++ Evaluation/IJB/IJB_release/IJBC_Evaluation_MS1MV2.py
@@ -293,30 +293,30 @@ colours = dict(zip(methods, sample_colours_from_colourmap(methods.shape[0], 'Set
 #x_labels = [1/(10**x) for x in np.linspace(6, 0, 6)]
 x_labels = [10**-6, 10**-5, 10**-4,10**-3, 10**-2, 10**-1]
 tpr_fpr_table = PrettyTable(['Methods'] + map(str, x_labels))
-fig = plt.figure()
+# fig = plt.figure()
 for method in methods:
     fpr, tpr, _ = roc_curve(label, scores[method])
     roc_auc = auc(fpr, tpr)
     fpr = np.flipud(fpr)
     tpr = np.flipud(tpr) # select largest tpr at same fpr
-    plt.plot(fpr, tpr, color=colours[method], lw=1, label=('[%s (AUC = %0.4f %%)]' % (method.split('-')[-1], roc_auc*100)))
+#    plt.plot(fpr, tpr, color=colours[method], lw=1, label=('[%s (AUC = %0.4f %%)]' % (method.split('-')[-1], roc_auc*100)))
     tpr_fpr_row = []
     tpr_fpr_row.append(method)
     for fpr_iter in np.arange(len(x_labels)):
         _, min_index = min(list(zip(abs(fpr-x_labels[fpr_iter]), range(len(fpr)))))
         tpr_fpr_row.append('%.4f' % tpr[min_index])
     tpr_fpr_table.add_row(tpr_fpr_row)
-plt.xlim([10**-6, 0.1])
-plt.ylim([0.3, 1.0])
-plt.grid(linestyle='--', linewidth=1)
-plt.xticks(x_labels) 
-plt.yticks(np.linspace(0.3, 1.0, 8, endpoint=True)) 
-plt.xscale('log')
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('ROC on IJB-C')
-plt.legend(loc="lower right")
-plt.show()
+#plt.xlim([10**-6, 0.1])
+#plt.ylim([0.3, 1.0])
+#plt.grid(linestyle='--', linewidth=1)
+#plt.xticks(x_labels) 
+#plt.yticks(np.linspace(0.3, 1.0, 8, endpoint=True)) 
+#plt.xscale('log')
+#plt.xlabel('False Positive Rate')
+#plt.ylabel('True Positive Rate')
+#plt.title('ROC on IJB-C')
+#plt.legend(loc="lower right")
+#plt.show()
 #fig.savefig('IJB-B.pdf')
 
 
